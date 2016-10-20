b3b687ab29194b5d7f5b9a254f6b83f7fcf6c8cf
ALIEN <matthias.moulin@gmail.com>
2016-10-20 11:36:25 +0200

    Updated documentation

--- MAGE/MAGE-doc/html/search/all_13.js
+++ MAGE/MAGE-doc/html/search/all_13.js
@@ -3,7 +3,7 @@ var searchData=
   ['wait',['Wait',['../classmage_1_1_semaphore.html#ae63599939b6bcc3939cbeddd7ffa5f66',1,'mage::Semaphore::Wait()'],['../classmage_1_1_condition_variable.html#abc279c54285145d981f409c2d110c85a',1,'mage::ConditionVariable::Wait()']]],
   ['waitforalltasks',['WaitForAllTasks',['../namespacemage.html#a7da78c39175a029c92ed42d8fb9f30af',1,'mage']]],
   ['warning',['Warning',['../namespacemage.html#a0eccd8065c75d5f2bf86b48a5be3bfe5',1,'mage']]],
-  ['windowproc',['WindowProc',['../namespacemage.html#a3b7426a2b3c10a52791dac481c2495a3',1,'mage']]],
+  ['windowproc',['WindowProc',['../namespacemage.html#a327147b1d76dcb41c17c23f9603d80c1',1,'mage']]],
   ['worker_5fsemaphore',['worker_semaphore',['../namespacemage.html#a17c3448f9fba7521d188d30bdfb77e33',1,'mage']]],
   ['write',['WRITE',['../namespacemage.html#afd76fcca37ce5c5b2227671290973c74aff9d196f4bda4079f3f1ce90bd644662',1,'mage']]]
 ];
